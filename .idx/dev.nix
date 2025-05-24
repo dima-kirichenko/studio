c4d23aad3c43880a3466b444ebc5e334d3634893
Dima Kirichenko <d.s.kirichenko@gmail.com>
2025-05-24 21:58:25 +0000

    feat: Update dev environment and add VS Code extensions
    - Updated Nixpkgs channel from 'stable-24.11' to 'unstable' in .idx/dev.nix.
    - Added 'vscodevim.vim' and 'RooVeterinaryInc.roo-code-nightly' to the list of VS Code extensions in .idx/dev.nix.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 {pkgs}: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
+  channel = "unstable"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
@@ -20,7 +20,8 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "vscodevim.vim"
+      "RooVeterinaryInc.roo-code-nightly"
     ];
     workspace = {
       onCreate = {
